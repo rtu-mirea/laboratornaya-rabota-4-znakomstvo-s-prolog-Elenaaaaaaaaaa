e0c7d4ea98577b643030d205fd49323cfb183371
Elenaaaaaaaaaa <56511891+Elenaaaaaaaaaa@users.noreply.github.com>
2019-12-28 20:48:48 +0300

    Update lab4.pl

--- lab4.pl
+++ lab4.pl
@@ -1,31 +1,18 @@
-domains
-  name=symbol
-predicates
-  nondeterm parent(name,name)
-  nondeterm brother(name,name)
-  nondeterm ancestor(name,name)
-  nondeterm sister(name,name)
-  nondeterm female(name)
-  nondeterm male(name)
-  nondeterm likes(name, name)
-  nondeterm can_buy(name,name)
-clauses
-  parent(ilia, marina).
-  parent(ilia, ira).
-  parent(elena, ivan).
-  parent(elena, alex).
-  parent(masha, elena).
-  male(ivan).
-  male(alex).
-  female(ira).
-  female(marina).
-  likes(ilia, food).
-  can_buy(alex, car).
+  parent(m1, w1).
+  parent(m1, w2).
+  parent(w3, m2).
+  parent(w3, m3).
+  parent(w3, w3).
+  male(m2).
+  male(m3).
+  female(w2).
+  female(w3).
+  likes(m3, something).
+  can_buy(m2, else).
+  
   /*RULE BROTHER*/
   brother(X,Y):-parent(Z,X),parent(Z,Y),male(X),male(Y).
   /*RULE ANCESTOR (PREDOK)*/
   ancestor(X,Z):-parent(X,Y),ancestor(Y,Z).
   /*RULE SISTER*/
   sister(X,Y):-parent(Z,X),parent(Z,Y),female(X),female(Y).
-goal
-  sister(marina,ira).
